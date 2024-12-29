1b339895b9d0b19fd2f4acf7ab13e43c99393edf
Tcthommyson <94145211+Tcthommyson@users.noreply.github.com>
2024-12-29 14:53:25 -0700

    Created Input Box for Athletes

--- sportsfrontend/src/components/Display.tsx
+++ sportsfrontend/src/components/Display.tsx
@@ -1,8 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Input } from "@chakra-ui/react";
+import { useState } from "react";
 export const Display = () => {
+  const [inputValue, setInputValue] = useState("");
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key == "Enter") {
+      console.log(inputValue);
+      setInputValue("");
+    }
+  };
   return (
-    <Box bg="tomato" w="100%" p="4">
-      no
+    <Box bgColor="gray" width="100%" padding="4" color="white">
+      <Input
+        placeholder="Enter name of athlete"
+        _placeholder={{ color: "white" }}
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
     </Box>
   );
 };
