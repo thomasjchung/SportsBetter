a66fffb6d270ede046df17e4eeebcc664709b00c
Tcthommyson <94145211+Tcthommyson@users.noreply.github.com>
2024-12-31 00:04:42 -0700

    Basic Arbitrage Functionality Done

--- sportsbackend.py
+++ sportsbackend.py
@@ -1,23 +1,28 @@
 import requests
 import json
 from decouple import config
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 API_KEY = config("API_KEY")
-
 sports_url = "https://api.the-odds-api.com/v4/sports"
-sports_params = {
-    "apiKey": API_KEY
-}
-sports_file = "sports_data.json"
+odds_url_template = "https://api.the-odds-api.com/v4/sports/{sport}/odds"
 
+sports_file = "sports_data.json"
+odds_file = "odds_data.json"
+
+@app.route('/api/sports', methods=['GET'])
 def getSports():
     try:
-        response = requests.get(sports_url, params = sports_params)
+        response = requests.get(sports_url, params = {"apiKey": API_KEY})
 
-        print("Quota Information:")
-        print(f"x-requests-remaining: {response.headers.get('x-requests-remaining', 'Not Available')}")
-        print(f"x-requests-used: {response.headers.get('x-requests-used', 'Not Available')}")
-        print(f"x-requests-last: {response.headers.get('x-requests-last', 'Not Available')}\n")
+        # print("Quota Information:")
+        # print(f"x-requests-remaining: {response.headers.get('x-requests-remaining', 'Not Available')}")
+        # print(f"x-requests-used: {response.headers.get('x-requests-used', 'Not Available')}")
+        # print(f"x-requests-last: {response.headers.get('x-requests-last', 'Not Available')}\n")
 
         if response.status_code == 200:
             sports_data = response.json()
@@ -26,27 +31,98 @@ def getSports():
             with open(sports_file, "w") as file:
                 json.dump(sports_data, file, indent=4)
 
-            print("Available Sports:")
-            for sport in sports_data:
-                print(f"Sport: {sport['title']} Description: {sport['description']}\n")
+            return jsonify({
+                "quota_info": {
+                    "x-requests-remaining": response.headers.get('x-requests-remaining', 'Not Available'),
+                    "x-requests-used": response.headers.get('x-requests-used', 'Not Available'),
+                    "x-requests-last": response.headers.get('x-requests-last', 'Not Available')
+                },
+                "sports": sports_data
+            })
+
+            # print("Available Sports:")
+            # for sport in sports_data:
+            #     print(f"Sport: {sport['title']} Description: {sport['description']}\n")
         else:
-            print(f"Failed to fetch sports. HTTP Status Code: {response.status_code}")
+            return jsonify({"error": "Failed to fetch sports", "status_code": response.status_code}), 400
     except requests.exceptions.RequestException as e:
-        print("An error occurred while making the API request:", e)
+        return jsonify({"error": str(e)}), 500
+
+def find_best_odds(stake):
+    with open("odds_data.json", "r") as file:
+        data = json.load(file)
+
+    best_odds = {}
+
+    home_arbitrage = []
+    away_arbitrage = []
+
+    home_bets = []
+    away_bets = []
+
+    best_bookmakers = {}
+
+    home_bookmaker = []
+    away_bookmaker = []
+
+    total_profit = []
+
+    for game in data:
+        home_team = game['home_team']
+        away_team = game['away_team']
+
+        for bookmaker in game['bookmakers']:
+            for market in bookmaker['markets']:
+                for outcome in market['outcomes']:
+                    name = outcome['name']
+                    price = outcome['price']
+
+                    if name not in best_odds or price > best_odds[name]:
+                        best_odds[name] = price
+                        best_bookmakers[name] = bookmaker['title']
+        
+        # Skip games without odds for either team
+        if home_team not in best_odds or away_team not in best_odds:
+            print(f"Skipping game: {home_team} vs {away_team} (no odds available)")
+            continue
+
+        total_implied_probability = (1 / best_odds[home_team]) + (1/best_odds[away_team])
+
+        if (total_implied_probability < 1):
+            home_arbitrage.append(home_team)
+            away_arbitrage.append(away_team)
+
+            home_bookmaker.append(best_bookmakers[home_team])
+            away_bookmaker.append(best_bookmakers[away_team])
+
+            total_profit.append((stake/total_implied_probability) - stake)
+
+            home_bets.append((stake * (1/best_odds[home_team])) / total_implied_probability)
+            away_bets.append((stake * (1/best_odds[away_team])) / total_implied_probability)
+
+        best_odds[home_team] = 0
+        best_odds[away_team] = 0
+
+    return home_arbitrage, away_arbitrage, home_bets, away_bets, total_profit, home_bookmaker, away_bookmaker
 
 
-odds_file = "odds_data.json"
-odds_url = "https://api.the-odds-api.com/v4/sports/basketball_nba/odds"
-odds_params = {
-    "apiKey": API_KEY,
-    "regions": "us",
-    "markets": "h2h",
-}
-
+@app.route('/api/odds', methods=['GET'])
 def getOdds():
+    sport = request.args.get('sport', 'basketball_nba')
+    regions = request.args.get('regions', 'us')
+    markets = request.args.get('markets', 'h2h')
+    stake = int(request.args.get('stake', '0'))
+
+    odds_url = odds_url_template.format(sport=sport)
+    params = {
+        "apiKey": API_KEY,
+        "regions": regions,
+        "markets": markets
+    }
+
     try:
         # Make the API request
-        response = requests.get(odds_url, params=odds_params)
+        response = requests.get(odds_url, params=params)
         
         # Print quota information
         print("Quota Information:")
@@ -62,70 +138,26 @@ def getOdds():
             with open(odds_file, "w") as file:
                 json.dump(odds_data, file, indent=4)
             
-            # Iterate through the games in the odds data
-            for game in odds_data:
-                # Iterate through the bookmakers for this game
-                for bookmaker in game.get('bookmakers', []):
-                    print(f"  Title: {bookmaker['title']}")
-                    print(f"  Last Update: {bookmaker['last_update']}")
-                    
-                    # Iterate through the markets for this bookmaker
-                    for market in bookmaker.get('markets', []):
-                        print(f"    Market: {market['key']}")
-                        
-                        # Iterate through the outcomes for this market
-                        for outcome in market.get('outcomes', []):
-                            print(f"      Name: {outcome['name']}")
-                            print(f"      Price: {outcome['price']}")
-                            # Check if 'point' exists and print it
-                            if 'point' in outcome:
-                                print(f"      Point: {outcome['point']}")
-                    print()  # Add a blank line after each bookmaker
-                print("-" * 40)  # Separator for games
+            home_arbitrage, away_arbitrage, home_bets, away_bets, total_profit, home_bookmaker, away_bookmaker = find_best_odds(stake)
+
+            if home_arbitrage:
+                return jsonify({
+                    "home_teams": home_arbitrage,
+                    "away_teams": away_arbitrage,
+                    "home_bets": home_bets,
+                    "away_bets": away_bets,
+                    "total_profit": total_profit,
+                    "home_bookmakers": home_bookmaker,
+                    "away_bookmakers": away_bookmaker
+                })
+            else:
+                return jsonify({
+                    "message": "No arbitrage opportunities found."
+                })
         else:
-            print(f"Failed to fetch odds. HTTP Status Code: {response.status_code}")
+            return jsonify({"error": "Failed to fetch odds", "status_code": response.status_code}), 400
     except requests.exceptions.RequestException as e:
-        print("An error occurred while making the API request:", e)
+        return jsonify({"error": str(e)}), 500
 
-def find_best_odds():
-    with open("odds_data.json", "r") as file:
-        data = json.load(file)
-
-    best_odds = {}
-
-    for game in data:
-        for bookmaker in game['bookmakers']:
-            for market in bookmaker['markets']:
-                for outcome in market['outcomes']:
-                    name = outcome['name']
-                    price = outcome['price']
-
-                    if name not in best_odds or price > best_odds[name]:
-                        best_odds[name] = price
-
-    return best_odds
-
-def calculate_arbitrage(best_odds):
-    with open("odds_data.json", "r") as file:
-        data = json.load(file)
-
-    for game in data:
-        implied_probabilities = []
-
-        home_team = game['home_team']
-        away_team = game['away_team']
-
-        implied_probabilities.append(1 / best_odds[home_team])
-        implied_probabilities.append(1 / best_odds[away_team])
-        
-        total_implied_probability = sum(implied_probabilities)
-
-        if (total_implied_probability < 1):
-            print(f"Arbitrage Opportunity Found between {home_team} and {away_team}!")
-        else:
-            print("No Arbitrage Opportunity")
-    
-# best_odds = dictionary for best odds for every team 
-# best_odds = find_best_odds()
-# calculate_arbitrage(best_odds)
-getSports()
+if __name__ == '__main__':
+    app.run(debug=True)
--- sportsfrontend/src/components/Display.tsx
+++ sportsfrontend/src/components/Display.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Input, Image, HStack, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Image,
+  HStack,
+  Heading,
+  Text,
+  Input,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import nbaImage from "../assets/nba.png";
 import nflImage from "../assets/nfl.png";
@@ -9,14 +17,63 @@ import wncaabImage from "../assets/NCAAWomenBasketball.webp";
 import bblImage from "../assets/bbl.png";
 import nhlImage from "../assets/nhl.png";
 
+type ArbitrageData = {
+  home_teams: string[];
+  away_teams: string[];
+  home_bets: number[];
+  away_bets: number[];
+  total_profit: number[];
+  home_bookmakers: string[];
+  away_bookmakers: string[];
+};
+
 export const Display = () => {
-  const [inputValue, setInputValue] = useState("");
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key == "Enter") {
-      console.log(inputValue);
-      setInputValue("");
+  const [sportValue, setSportValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [arbitrageData, setArbitrageData] = useState<
+    ArbitrageData | string | null
+  >(null);
+
+  const [stakeValue, setStakeValue] = useState("");
+
+  const fetchArbitrageData = async () => {
+    if (!sportValue) {
+      alert("Plase select a sport first!");
+      return;
+    }
+
+    if (!stakeValue || isNaN(Number(stakeValue))) {
+      alert("Please enter a valid stake value!");
+      return;
+    }
+
+    const stakeNum = Number(stakeValue);
+
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:5000/api/odds?sport=${sportValue}&stake=${stakeNum}`
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        if (data.home_teams && data.home_teams.length > 0) {
+          setArbitrageData(data);
+        } else {
+          setArbitrageData("No Arbitrage Found!");
+        }
+      } else {
+        console.error(data.error || "Failed to fetch data");
+        setArbitrageData("An error occurred while fetching arbitrage data.");
+      }
+    } catch (error) {
+      console.error("Error fetching arbitrage data:", error);
+      setArbitrageData("An error occurred. Please try again.");
+    } finally {
+      setLoading(false); // Stop loading
     }
   };
+
   return (
     <Box bgColor="gray" width="100%" padding="4" color="white">
       {/* <Input
@@ -27,13 +84,25 @@ export const Display = () => {
         onKeyDown={handleKeyDown}
       /> */}
       <HStack gap="5">
-        <Button padding="1" size="2xl">
+        <Button
+          padding="1"
+          size="2xl"
+          onClick={() => setSportValue("basketball_nba")}
+        >
           <Image src={nbaImage} alt="NBA" boxSize="50px" objectFit="contain" />
         </Button>
-        <Button padding="1" size="2xl">
+        <Button
+          padding="1"
+          size="2xl"
+          onClick={() => setSportValue("americanfootball_nfl")}
+        >
           <Image src={nflImage} alt="NFL" boxSize="50px" objectFit="contain" />
         </Button>
-        <Button padding="1" size="2xl">
+        <Button
+          padding="1"
+          size="2xl"
+          onClick={() => setSportValue("icehockey_nhl")}
+        >
           <Image src={nhlImage} alt="NHL" boxSize="50px" objectFit="contain" />
         </Button>
         <Button padding="1" size="2xl">
@@ -42,9 +111,14 @@ export const Display = () => {
             alt="NCAAB"
             boxSize="50px"
             objectFit="contain"
+            onClick={() => setSportValue("basketball_ncaab")}
           />
         </Button>
-        <Button padding="1" size="2xl">
+        <Button
+          padding="1"
+          size="2xl"
+          onClick={() => setSportValue("americanfootball_ncaaf")}
+        >
           <Image
             src={ncaafImage}
             alt="NCAAF"
@@ -52,7 +126,11 @@ export const Display = () => {
             objectFit="contain"
           />
         </Button>
-        <Button padding="1" size="2xl">
+        <Button
+          padding="1"
+          size="2xl"
+          onClick={() => setSportValue("basketball_euroleague")}
+        >
           <Image
             src={euroleagueImage}
             alt="EuroLeague Basketball"
@@ -60,7 +138,11 @@ export const Display = () => {
             objectFit="contain"
           />
         </Button>
-        <Button padding="1" size="2xl">
+        <Button
+          padding="1"
+          size="2xl"
+          onClick={() => setSportValue("basketball_wncaab")}
+        >
           <Image
             src={wncaabImage}
             alt="WNCAAB"
@@ -68,11 +150,54 @@ export const Display = () => {
             objectFit="contain"
           />
         </Button>
-        <Button padding="1" size="2xl">
+        <Button
+          padding="1"
+          size="2xl"
+          onClick={() => setSportValue("cricket_big_bash")}
+        >
           <Image src={bblImage} alt="BBL" boxSize="50px" objectFit="contain" />
         </Button>
       </HStack>
-      <Heading>Choose a Sport!</Heading>
+      <Heading>Select Your Sport!</Heading>
+
+      <HStack mt="4">
+        <Input
+          placeholder="Enter Stake"
+          value={stakeValue}
+          onChange={(e) => setStakeValue(e.target.value)} // Convert to number and handle invalid input
+          _placeholder={{ color: "white" }}
+          color="black"
+        />
+        <Button color="black" onClick={fetchArbitrageData} disabled={loading}>
+          {loading ? "Loading..." : "Submit!"}
+        </Button>
+      </HStack>
+
+      {arbitrageData && (
+        <Box mt="4">
+          {typeof arbitrageData === "string" ? (
+            <Text fontSize="lg" color="red">
+              {arbitrageData}
+            </Text>
+          ) : (
+            <Box>
+              <Heading size="md">
+                Arbitrage Opportunities with stake {stakeValue}:
+              </Heading>
+              {arbitrageData.home_teams.map((homeTeam, index) => (
+                <Text key={index}>
+                  {homeTeam} ({arbitrageData.home_bookmakers[index]}-
+                  {arbitrageData.home_bets[index]}) vs{" "}
+                  {arbitrageData.away_teams[index]} (
+                  {arbitrageData.away_bookmakers[index]}-
+                  {arbitrageData.away_bets[index]}) -- Total Profit = (
+                  {arbitrageData.total_profit[index]})
+                </Text>
+              ))}
+            </Box>
+          )}
+        </Box>
+      )}
     </Box>
   );
 };
